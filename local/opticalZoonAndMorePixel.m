903088b009b820bc6ecf83905679063bbb779b8f
Zheng Lyu <zhenglyu@vistalab-compute-1.c.som-vistalab.internal>
2019-02-25 10:34:47 -0800

    add the tutorials file to try some ideas

---
+++ local/opticalZoonAndMorePixel.m
@@ -0,0 +1,12 @@
+%%
+%%
+ieInit;
+
+%%
+inFile = fullfile('/scratch', 'ZhengLyu', 'super_resolution_pbrt_scenes',...
+                                        'optical_image', 'colorful.mat');
+                                    
+load(inFile); % 
+
+%%
+
