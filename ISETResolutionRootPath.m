8cf8f6f00b855224e0cad3819e9c9abe2e4adebb
Zheng Lyu <zhenglyu@gray.stanford.edu>
2019-02-27 14:42:13 -0800

    add the rootpath

---
+++ ISETResolutionRootPath.m
@@ -0,0 +1,21 @@
+function rootPath=ISETResolutionRootPath()
+% Return the path to the root iset directory
+%
+% This function must reside in the directory at the base of the pbrt2ISET
+% directory structure.  It is used to determine the location of various
+% sub-directories.
+% 
+% Example:
+%   fullfile(p2iRootPath,'data')
+
+rootPath=which('ISETResolutionRootPath');
+% try
+%     ls('-la','/opt/toolboxes')
+% catch
+%     error('Error on ls of /opt/toolboxes');
+% end
+% tbLocateToolbox('iset3d')
+
+[rootPath,fName,ext]=fileparts(rootPath);
+
+return
