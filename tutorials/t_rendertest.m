4bbe8c9ff26ce6244ba4fba826a202e5554cf5dd
Zheng Lyu <zhenglyu@gray.stanford.edu>
2019-03-11 11:31:20 -0700

    add the rendertest script as an attempt to use PBRT to generate scene

--- tutorials/t_rendertest.m
+++ tutorials/t_rendertest.m
@@ -8,23 +8,23 @@ if ~piDockerExists, piDockerConfig; end
 %% Read the file
 
 % The teapot is our test file
-% /scratch/zhenglyu/pbrt-v3-scenes/landscape
-inFile = fullfile('/scratch', 'zhenglyu', 'pbrt-v3-scenes',...
+% /scratch/zhenglyu/pbrt-v3-scene-ZL/
+inFile = fullfile('/scratch', 'zhenglyu', 'pbrt-v3-scene-ZL',...
                                         'landscape', 'view-0.pbrt');
 recipe = piRead(inFile);
 
 % The output will be written here
 sceneName = 'landscape';
 %/scratch/zhenglyu/renderedScene/
-outFile = fullfile('/scratch','zhenglu','renderedScene', ...
-                    sceneName,'landscape_v1.pbrt');
+outFile = fullfile('/scratch','zhenglyu','renderedScene', ...
+                    sceneName,'landscape_v3.pbrt');
 recipe.set('outputFile',outFile);
 %% Set up the render quality
 
 % There are many different parameters that can be set.
-recipe.set('film resolution',[4096 2304]);
-recipe.set('pixel samples',1024);
-recipe.set('max depth',1); % Number of bounces
+recipe.set('film resolution',[8192 4608]);
+recipe.set('pixel samples', 1024);
+% recipe.set('max depth',20); % Number of bounces
 
 %% Render
 piWrite(recipe);
@@ -32,14 +32,14 @@ piWrite(recipe);
 %%  This is a pinhole case.  So we are rendering a scene.
 
 [scene, result] = piRender(recipe);
-
+%%
 ieAddObject(scene); sceneWindow;
-scene = sceneSet(scene,'gamma',0.5);
+scene = sceneSet(scene,'gamma',1);
 
 % Notice that we also computed the depth map
 scenePlot(scene,'depth map');
 %%
-outScene = fullfile('/scratch','zhenglu','renderedScene', ...
-                    sceneName,'landscapeTest_v1.m');
-save(outScene, scene);
+outScene = fullfile('/scratch','zhenglyu','renderedScene', ...
+                    sceneName,'landscape_v3_v1.m');
+save(outScene, 'scene');
 %% END
